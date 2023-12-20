16e6def602f7622016ed7acaf5da7ae4456269e1
Will Bush <will.g.bush@gmail.com>
2023-12-20 00:56:18 -0600

    use plasma5 installer as base to simplify iso.nix

--- iso.nix
+++ iso.nix
@@ -1,27 +1,11 @@
 { modulesPath, pkgs, ... }:
 {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-plasma5.nix"
   ];
 
-  isoImage.edition = "plasma5";
-
-  services = {
-    xserver = {
-      desktopManager.plasma5.enable = true;
-
-      # Automatically login as nixos.
-      displayManager = {
-        sddm.enable = true;
-        autoLogin = {
-          enable = true;
-          user = "nixos";
-        };
-      };
-    };
-    # Enables copy / paste when running in a KVM with spice.
-    spice-vdagentd.enable = true;
-  };
+  # Enables copy / paste when running in a KVM with spice.
+  services.spice-vdagentd.enable = true;
 
   users.users.nixos.shell = pkgs.zsh;
   programs.zsh.enable = true;
@@ -50,25 +34,4 @@
       };
     };
   };
-
-  system.activationScripts.installerDesktop =
-    let
-
-      # Comes from documentation.nix when xserver and nixos.enable are true.
-      manualDesktopFile =
-        "/run/current-system/sw/share/applications/nixos-manual.desktop";
-
-      homeDir = "/home/nixos/";
-      desktopDir = homeDir + "Desktop/";
-
-    in
-    ''
-      mkdir -p ${desktopDir}
-      chown nixos ${homeDir} ${desktopDir}
-
-      ln -sfT ${manualDesktopFile} ${desktopDir + "nixos-manual.desktop"}
-      ln -sfT ${pkgs.alacritty}/share/applications/Alacritty.desktop ${desktopDir + "Alacritty.desktop"}
-      ln -sfT ${pkgs.calamares-nixos}/share/applications/io.calamares.calamares.desktop ${desktopDir + "io.calamares.calamares.desktop"}
-      ln -sfT ${pkgs.gparted}/share/applications/gparted.desktop ${desktopDir + "gparted.desktop"}
-    '';
 }
