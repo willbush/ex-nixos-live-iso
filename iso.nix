a225310bb6132c7e55dc575c08aac1839cef7a10
Will Bush <will.g.bush@gmail.com>
2023-12-20 22:55:03 -0600

    fix path to init.vim

--- iso.nix
+++ iso.nix
@@ -32,7 +32,7 @@
 
       neovim = {
         enable = true;
-        extraConfig = builtins.readFile ../../nvim/init.vim;
+        extraConfig = builtins.readFile ./nvim/init.vim;
       };
     };
   };
